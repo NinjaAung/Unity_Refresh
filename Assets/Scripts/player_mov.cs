b250dac84bae81184ba2e5590879a6f70243d456
ninjaaung <ninja.aung02@gmail.com>
2020-05-21 10:06:31 -0700

    StartingG round and player added

---
+++ Assets/Scripts/player_mov.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class player_mov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
