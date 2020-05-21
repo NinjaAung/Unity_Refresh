18b93701ac733cb287de45f66651b4d739bb589b
ninjaaung <ninja.aung02@gmail.com>
2020-05-21 12:00:56 -0700

    Added CameraScript

--- Assets/Scripts/player_mov.cs
+++ Assets/Scripts/player_mov.cs
@@ -2,15 +2,45 @@
 
 public class player_mov : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float forward_force = 0.0f;
+    public float side_force = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+    }
+
+    // Fixed Update is prefered for calculating physics
+    void FixedUpdate() 
+    {   
+        if ( Input.GetKey(KeyCode.W) )
+        {
+            rb.AddForce(0,0, forward_force * Time.deltaTime);
+            Debug.Log("Moving Forwards");
+        }
+        if ( Input.GetKey(KeyCode.S) )
+        {
+            rb.AddForce(0,0, -forward_force * Time.deltaTime);
+            Debug.Log("Moving Backwards");
+        }
+        if ( Input.GetKey(KeyCode.D) )
+        {
+            rb.AddForce(side_force * Time.deltaTime, 0, 0);
+            Debug.Log("Moving Right");
+        }
+        if ( Input.GetKey(KeyCode.A) )
+        {
+            rb.AddForce(-side_force * Time.deltaTime, 0, 0);
+            Debug.Log("Moving Left");
+        }
+
     }
 }
